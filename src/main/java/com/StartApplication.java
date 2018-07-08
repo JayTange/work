cf6eb7f61e67a312ee55e80fe9960ce5288577a3
tangj <840391701@qq.com>
2018-07-08 16:09:40 +0800

    first commit

---
+++ src/main/java/com/StartApplication.java
@@ -0,0 +1,62 @@
+package com;
+
+import com.json.Constants;
+import com.json.GsonUtil;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class StartApplication {
+
+    private static String menu = "please input cmd:" +
+            "1:all RailModel£º" +
+            "2£ºgrade" +
+            "3£ºweiche"+
+            "4: Abzweigung" +
+            "5: Fest" +
+            "6: exit program";
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.err.println(menu);
+            String cmdStr = scanner.nextLine();
+            switch (cmdStr){
+                case "1":
+                    System.out.println(GsonUtil.toJsonString(Constants.grade));
+                    System.out.println(GsonUtil.toJsonString(Constants.weiche));
+                    System.out.println(GsonUtil.toJsonString(Constants.Fest));
+                    System.out.println(GsonUtil.toJsonString(Constants.Abzweigung));
+                    break;
+                case "2":
+                    System.out.println("-----------");
+                    System.out.println(GsonUtil.toJsonString(Constants.grade));
+                    System.out.println("-----------");
+                    break;
+                case "3":
+                    System.out.println("-----------");
+                    System.out.println(GsonUtil.toJsonString(Constants.weiche));
+                    System.out.println("-----------");
+                    break;
+                case "4":
+                    System.out.println("-----------");
+                    System.out.println(GsonUtil.toJsonString(Constants.Abzweigung));
+                    System.out.println("-----------");
+                    break;
+                case "5":
+                    System.out.println("-----------");
+                    System.out.println(GsonUtil.toJsonString(Constants.Fest));
+                    System.out.println("-----------");
+                    break;
+                case "6":
+                    System.exit(-1);
+                default:
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ src/main/java/com/json/Constants.java
@@ -0,0 +1,29 @@
+package com.json;
+
+/**
+ * @author tangj
+ * @date 2018/7/8 15:13
+ */
+public class Constants {
+
+    /**
+     * 直道
+     */
+    public static RailModel grade = new RailModel("grade","");
+
+    /**
+     * 弯道
+     */
+    public static RailModel weiche = new RailModel("weiche","");
+    /**
+     * 岔路
+     */
+    public static RailModel Abzweigung = new RailModel("Abzweigung","");
+    /**
+     * 固定类型
+     */
+    public static RailModel Fest = new RailModel("Fest","");
+
+
+
+}
